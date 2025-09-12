49f16af5659ba011ce7cb5ae86099a36d4d06fe6
CycyCutieePatootie <cyrilinigo1504@gmail.com>
2025-09-12 16:46:36 +0800

    done sidebar, main content continue

--- src/features/Dashboard/dashboard.jsx
+++ src/features/Dashboard/dashboard.jsx
@@ -19,9 +19,45 @@ function Dashboard() {
         />
         <div className="content-wrapper">
           <main className="main-content">
-            <h1>Dashboard</h1>
-            <p>Welcome to your dashboard!</p>
-            {/* Add your dashboard content here */}
+            <div className="row1">
+                    <p>Dashboard</p> <br></br>
+
+              <div className="col1">
+                <div className="subrow1">
+                  {/* Content for subrow1 */}
+                </div>
+                <div className="subrow2">
+                  {/* Content for subrow2 */}
+                </div>
+                <div className="subrow3">
+                  <div className="subcol1">
+                    {/* Content for subcol1 */}
+                  </div>
+                  <div className="subcol2">
+                    {/* Content for subcol2 */}
+                  </div>
+                </div>
+              </div>
+              <div className="col2">
+                <div className="subrow1">
+                  {/* Content for col2 subrow1 */}
+                </div>
+                <div className="subrow2">
+                  {/* Content for col2 subrow2 */}
+                </div>
+              </div>
+            </div>
+            <div className="row2">
+              <div className="col1">
+                {/* Content for row2 col1 */}
+              </div>
+              <div className="col2">
+                {/* Content for row2 col2 */}
+              </div>
+              <div className="col3">
+                {/* Content for row2 col3 */}
+              </div>
+            </div>
           </main>
         </div>
       </div>
